e70b39d1e84e35cf094b3d881d19d4143d90d8cf
DaryaniLeena <daryani.l@husky.neu.edu>
2021-03-21 15:47:55 -0700

    changes in script

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -2,7 +2,7 @@
 # Will need github id and pwd
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install nodejs git nginx python3-pip -y
+sudo apt-get install nodejs git nginx python3-pip python3-venv gunicorn -y
 sudo apt install npm
 cd /home/ubuntu
 git clone https://DaryaniLeena:Gitpass03@github.com/DaryaniLeena/csye7220MidTerm.git
@@ -14,15 +14,12 @@ sudo cp /home/ubuntu/csye7220MidTerm/uberfe/uber.nginx /etc/nginx/sites-availabl
 sudo chmod 777 /etc/nginx/sites-available/uber.nginx
 sudo ln -s /etc/nginx/sites-available/uber.nginx /etc/nginx/sites-enabled/uber.nginx
 sudo systemctl reload nginx
-
-
-sudo cp /home/ubuntu/csye7220MidTerm/uberbe/uberbe.service /etc/systemd/system/uberbe.service
-# python3 -m venv .venv
-# source .venv/bin/activate
+python3 -m /home/ubuntu/csye7220MidTerm/uberbe/venv env
+source /home/ubuntu/csye7220MidTerm/uberbe/env/bin/activate
+# cd /home/ubuntu/csye7220MidTerm/uberfe
+# sudo rm /var/www/html -r
+# sudo mv ./build /var/www/html
 cd /home/ubuntu/csye7220MidTerm/uberbe
-pip3 install gunicorn
 pip3 install -r /home/ubuntu/csye7220MidTerm/uberbe/requirements.txt
-sudo systemctl daemon-reload
-sudo systemctl start uberbe
 # gunicorn --bind 0.0.0.0 main:app -D
-# gunicorn -w 4 -b 0.0.0.0:5000 --chdir /home/ubuntu/csye7220MidTerm/uberbe wsgi:app
+gunicorn -w 4 -b 0.0.0.0:5000 --chdir /home/ubuntu/csye7220MidTerm/uberbe wsgi:app
